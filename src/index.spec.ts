26742927987c3a00e6ca09f5780e2bb96bf69ab4
Sudarsan Balaji <sudarsan.balaji@artfuldev.com>
2023-08-11 00:51:30 +0100

    Simplify coerce

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,76 +1,69 @@
 import { expect } from "chai";
-import { coerce, match } from ".";
+import { coerce } from ".";
 
 describe("coerce", () => {
-  describe("match", () => {
-    context("given a matcher", () => {
-      const _match = (factory: () => Promise<number>) =>
-        coerce(factory()).then(
-          match(
-            () => "pending",
-            (result) => `resolved: ${result}`,
-            (reason) => `rejected: ${reason.message}`
-          )
-        );
+  context("given a matcher", () => {
+    const match = coerce(
+      () => "pending",
+      (result) => `resolved: ${result}`,
+      (reason) => `rejected: ${reason.message}`
+    );
 
-      context("and a promise that eventually", () => {
-        const delay = 1000;
+    context("and a promise that eventually", () => {
+      const delay = 1000;
 
-        context("rejects", () => {
-          const rejects = () =>
-            new Promise<number>((_, reject) =>
-              setTimeout(() => reject(new Error()), delay)
-            );
+      context("rejects", () => {
+        const rejects = () =>
+          new Promise<number>((_, reject) =>
+            setTimeout(() => reject(new Error()), delay)
+          );
 
-          it("should return the pending match", async () => {
-            expect(await _match(rejects)).to.equal("pending");
-          });
-
-          it("should not wait for the promise to settle", async () => {
-            const started = Date.now();
-            await _match(rejects);
-            const ended = Date.now();
-            const elapsed = ended - started;
-            expect(elapsed).to.be.lessThan(delay);
-          });
+        it("should return the pending match", async () => {
+          expect(await match(rejects())).to.equal("pending");
         });
 
-        context("resolves", () => {
-          const resolves = () =>
-            new Promise<number>((resolve) =>
-              setTimeout(() => resolve(1), delay)
-            );
-
-          it("should return the pending match", async () => {
-            expect(await _match(resolves)).to.equal("pending");
-          });
-
-          it("should not wait for the promise to settle", async () => {
-            const started = Date.now();
-            await _match(resolves);
-            const ended = Date.now();
-            const elapsed = ended - started;
-            expect(elapsed).to.be.lessThan(delay);
-          });
+        it("should not wait for the promise to settle", async () => {
+          const started = Date.now();
+          await match(rejects());
+          const ended = Date.now();
+          const elapsed = ended - started;
+          expect(elapsed).to.be.lessThan(delay);
         });
       });
 
-      context("and a promise that's resolved", () => {
-        const value = 100;
-        const resolved = () => Promise.resolve(value);
+      context("resolves", () => {
+        const resolves = () =>
+          new Promise<number>((resolve) => setTimeout(() => resolve(1), delay));
 
-        it("should return the resolved match", async () => {
-          expect(await _match(resolved)).to.equal(`resolved: ${value}`);
+        it("should return the pending match", async () => {
+          expect(await match(resolves())).to.equal("pending");
+        });
+
+        it("should not wait for the promise to settle", async () => {
+          const started = Date.now();
+          await match(resolves());
+          const ended = Date.now();
+          const elapsed = ended - started;
+          expect(elapsed).to.be.lessThan(delay);
         });
       });
+    });
 
-      context("and a promise that's rejected", () => {
-        const message = "error message";
-        const rejected = () => Promise.reject(new Error(message));
+    context("and a promise that's resolved", () => {
+      const value = 100;
+      const resolved = () => Promise.resolve(value);
 
-        it("should return the rejected match", async () => {
-          expect(await _match(rejected)).to.equal(`rejected: ${message}`);
-        });
+      it("should return the resolved match", async () => {
+        expect(await match(resolved())).to.equal(`resolved: ${value}`);
+      });
+    });
+
+    context("and a promise that's rejected", () => {
+      const message = "error message";
+      const rejected = () => Promise.reject(new Error(message));
+
+      it("should return the rejected match", async () => {
+        expect(await match(rejected())).to.equal(`rejected: ${message}`);
       });
     });
   });
--- src/index.ts
+++ src/index.ts
@@ -1,43 +1,14 @@
 const Pending = Symbol("pending");
-type Pending = [typeof Pending];
-const pending: Pending = [Pending];
 
-const Resolved = Symbol("resolved");
-type Resolved<T> = [typeof Resolved, T];
-const resolved = <T>(result: T): Resolved<T> => [Resolved, result];
-
-const Rejected = Symbol("rejected");
-type Rejected<R = Error> = [typeof Rejected, R];
-const rejected = <B = Error>(reason: B): Rejected<B> => [Rejected, reason];
-
-const Coerced = Symbol("coerced");
-
-export type Coerced<T, R = Error> = {
-  [Coerced]: Pending | Resolved<T> | Rejected<R>;
-};
-
-export const coerce = <T, R = Error>(
-  value: Promise<T>
-): Promise<Coerced<T, R>> =>
-  Promise.race([value, pending])
-    .then((result) => (result === pending ? pending : resolved(result as T)))
-    .catch(rejected)
-    .then((coerced) => ({ [Coerced]: coerced }));
-
-export const match =
+export const coerce =
   <T, A, R = Error>(
     pending: () => A,
     resolved: (result: T) => A,
     rejected: (reason: R) => A
   ) =>
-  (coerced: Coerced<T, R>): A => {
-    const value = coerced[Coerced];
-    switch (value[0]) {
-      case Pending:
-        return pending();
-      case Resolved:
-        return resolved(value[1]);
-      case Rejected:
-        return rejected(value[1]);
-    }
-  };
+  (value: Promise<T>): Promise<A> =>
+    Promise.race([value, Pending])
+      .then((result) =>
+        result === Pending ? pending() : resolved(result as T)
+      )
+      .catch(rejected);
